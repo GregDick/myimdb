3d4d404e534e0d7263282d1af2ab6375d78b5ad3
royschuyler <royschuyler@yahoo.com>
2015-05-11 15:09:17 -0500

    started $.get function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var API_URL = 'http://www.omdbapi.com/?';
+var div = $('div').html('hey');
+
+var omdb_URL = "http://www.omdbapi.com/?";
+var firebase_URL = "https://movie-appp.firebaseio.com/";
+
+$.get(
+
+
