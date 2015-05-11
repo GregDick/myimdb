bd8492f242dfd6fe6a2fb4a4bccbd0ae5a8a43e3
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-05-11 15:38:32 -0500

    added get movie api function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-var API_URL = 'http://www.omdbapi.com/?';
+var omdb_URL = 'http://www.omdbapi.com/?';
+var $searchForm = $('.search-form');
+var $searchBar = $('input[name=search]')[0];
+
+
+//function to retrieve movie JSON file and add to html
+$searchForm.on('submit', function(){
+  var movie = $searchBar.value;
+  var url = omdb_URL + "t=" + movie + "&r=json";
+  console.log(url);
+  $.get(url, function(data){
+    console.log(data);
+  })
+  return false;
+})
