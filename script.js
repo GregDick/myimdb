27b6665e219a7e96affc12a34864e73891eeea1e
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-05-11 17:49:55 -0500

    working search bar

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ $searchForm.on('submit', function(){
   console.log(url);
   $.get(url, function(data){
     console.log(data);
+    addMovieDetail(data);
   })
   return false;
 })
+
+
+//function to add JSON data to html
+function addMovieDetail(data){
+  var $target = $(".movie-details");
+  $target.empty();
+  $target.append("<img src=" + data.Poster + "></img>")
+  $target.append("<h1>" + data.Title + "</h1>");
+  $target.append("<h2> Year: " + data.Year + "</h2>");
+  $target.append("<h2> IMDB Rating: " + data.imdbRating + "</h2>");
+}
+
+
+
+
